e2f144430076c92fa86232fb24562e0c7398c59d
Peter Arato <it.arato@gmail.com>
2026-02-12 21:05:26 -0500

    Hash-object

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@
 extern crate log;
 
 use clap::Parser;
-use flate2::read::ZlibDecoder;
+use flate2::{Compression, read::ZlibDecoder, write::ZlibEncoder};
+use sha1::{Digest, Sha1};
 use std::{
     fs::{self, File},
-    io::Read,
+    io::{Read, Write},
     path::Path,
 };
+
+use crate::common::bytes_to_string;
+
+mod common;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -16,6 +21,9 @@ struct Args {
 
     #[arg(short = 'p', long)]
     object_hash: Option<String>,
+
+    #[arg(short = 'w', long)]
+    file_path: Option<String>,
 }
 
 fn main() {
@@ -32,6 +40,7 @@ fn main() {
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             info!("Initialized git directory")
         }
+
         "cat-file" => {
             let hash = args.object_hash.unwrap();
             let prefix = &hash[0..2];
@@ -53,6 +62,39 @@ fn main() {
             let content = str::from_utf8(&content_buf[content_start_index..]).unwrap();
             print!("{}", content);
         }
+
+        "hash-object" => {
+            let mut content_suffix = fs::read_to_string(&args.file_path.unwrap())
+                .unwrap()
+                .as_bytes()
+                .to_vec();
+            let mut content = format!("blob {}\0", content_suffix.len())
+                .as_bytes()
+                .to_vec();
+            content.append(&mut content_suffix);
+
+            let mut hasher = Sha1::new();
+            hasher.update(&content);
+            let hash = hasher.finalize();
+            let hash_str = bytes_to_string(&hash[..]);
+
+            println!("{}", hash_str);
+
+            let prefix = &hash_str[0..2];
+            let filename = &hash_str[2..];
+            let folderpath_raw = format!(".git/objects/{}", prefix);
+            let folderpath = Path::new(&folderpath_raw);
+            fs::create_dir_all(folderpath).unwrap();
+
+            let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+            encoder.write_all(&content).unwrap();
+            let content_encoded = encoder.finish().unwrap();
+
+            let filepath_raw = format!(".git/objects/{}/{}", prefix, filename);
+            let filepath = Path::new(&filepath_raw);
+            fs::write(filepath, content_encoded).unwrap();
+        }
+
         other => {
             error!("unknown command: {}", other)
         }
