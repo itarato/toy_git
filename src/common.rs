c0dfebe16c745052b073ae9b2983293d6b14ed45
Peter Arato <it.arato@gmail.com>
2026-02-14 08:57:15 -0500

    read tree

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,130 @@
+use std::{
+    fs::{self, File},
+    io::Read,
+};
+
+use flate2::read::ZlibDecoder;
+
+use crate::reader::Reader;
+
 pub(crate) fn bytes_to_string(bytes: &[u8]) -> String {
     bytes
         .iter()
         .map(|b| format!("{:02x}", b))
         .collect::<Vec<_>>()
         .join("")
+}
+
+#[derive(Debug)]
+pub(crate) struct Hash {
+    pub(crate) hash: String,
+}
+
+impl Hash {
+    pub(crate) fn new(hash: String) -> Self {
+        Self { hash }
+    }
+
+    pub(crate) fn folder_path(&self) -> String {
+        let prefix = &self.hash[0..2];
+        format!(".git/objects/{}", prefix)
+    }
+
+    pub(crate) fn file_path(&self) -> String {
+        let prefix = &self.hash[0..2];
+        let filename = &self.hash[2..];
+        format!(".git/objects/{}/{}", prefix, filename)
+    }
+
+    pub(crate) fn read(&self) -> Entry {
+        let file = File::open(self.file_path()).unwrap();
+        let mut decoder = ZlibDecoder::new(file);
+        let mut content_buf = vec![];
+        decoder.read_to_end(&mut content_buf).unwrap();
+
+        let mut reader = Reader::new(&content_buf[..]);
+        let kind = str::from_utf8(reader.pop_while(|c| c != &b' ')).unwrap();
+
+        reader.pop(); // space
+        let _payload_len =
+            usize::from_str_radix(str::from_utf8(reader.pop_while(|c| c != &0)).unwrap(), 10)
+                .unwrap();
+        reader.pop(); // \0
+
+        match kind {
+            "blob" => {
+                let content = str::from_utf8(reader.pop_all()).unwrap().to_string();
+                Entry::File { content }
+            }
+            "tree" => {
+                let mut entries = vec![];
+
+                while !reader.is_empty() {
+                    // tree <size>\0
+                    // <mode> <name>\0<20_byte_sha>
+                    // <mode> <name>\0<20_byte_sha>
+                    let perm = str::from_utf8(reader.pop_while(|c| c != &b' '))
+                        .unwrap()
+                        .to_string();
+                    reader.pop(); // space
+                    let filename = str::from_utf8(reader.pop_while(|c| c != &0))
+                        .unwrap()
+                        .to_string();
+                    reader.pop(); // \0
+                    let hash_bytes = reader.popn(20);
+                    let hash_str = bytes_to_string(hash_bytes);
+                    let hash = Hash::new(hash_str);
+
+                    entries.push(TreeEntry {
+                        perm,
+                        filename,
+                        hash,
+                    });
+                }
+
+                Entry::Tree { entries }
+            }
+            other => {
+                error!("Unrecognized entry indicator: {}", other);
+                panic!()
+            }
+        }
+    }
+}
+
+pub(crate) fn read_file_into_encoded_blob(file_path: &str) -> Vec<u8> {
+    let mut content_suffix = fs::read_to_string(file_path).unwrap().as_bytes().to_vec();
+    let mut content = format!("blob {}\0", content_suffix.len())
+        .as_bytes()
+        .to_vec();
+    content.append(&mut content_suffix);
+
+    content
+}
+
+#[derive(Debug)]
+pub(crate) struct TreeEntry {
+    pub(crate) perm: String,
+    pub(crate) filename: String,
+    pub(crate) hash: Hash,
+}
+
+impl TreeEntry {
+    pub(crate) fn perm_to_string(&self) -> &str {
+        match self.perm.as_str() {
+            "100644" => "blob",
+            "040000" => "tree",
+            other => {
+                error!("Perm type not implemented: {}", other);
+                panic!()
+            }
+        }
+    }
+}
+
+pub(crate) enum Entry {
+    Tree { entries: Vec<TreeEntry> },
+    File { content: String },
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,23 @@
 extern crate log;
 
 use clap::Parser;
-use flate2::{Compression, read::ZlibDecoder, write::ZlibEncoder};
+use flate2::{Compression, write::ZlibEncoder};
 use sha1::{Digest, Sha1};
-use std::{
-    fs::{self, File},
-    io::{Read, Write},
-    path::Path,
-};
+use std::{fs, io::Write};
 
-use crate::common::bytes_to_string;
+use crate::common::{Entry, Hash, bytes_to_string, read_file_into_encoded_blob};
 
 mod common;
+mod reader;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
     command: String,
+    object_hash: Option<String>,
 
     #[arg(short = 'p', long)]
-    object_hash: Option<String>,
+    cat_file_hash: Option<String>,
 
     #[arg(short = 'w', long)]
     file_path: Option<String>,
@@ -41,59 +39,43 @@ fn main() {
             info!("Initialized git directory")
         }
 
-        "cat-file" => {
-            let hash = args.object_hash.unwrap();
-            let prefix = &hash[0..2];
-            let filename = &hash[2..];
-            let filepath_raw = format!(".git/objects/{}/{}", prefix, filename);
-            let filepath = Path::new(&filepath_raw);
-            let file = File::open(filepath).unwrap();
-            let mut decoder = ZlibDecoder::new(file);
-            let mut content_buf = vec![];
-            decoder.read_to_end(&mut content_buf).unwrap();
-
-            let mut content_start_index = 0;
-            for i in 0..content_buf.len() {
-                if content_buf[i] == 0 {
-                    content_start_index = i + 1;
-                }
-            }
-
-            let content = str::from_utf8(&content_buf[content_start_index..]).unwrap();
-            print!("{}", content);
-        }
+        "cat-file" => match Hash::new(args.cat_file_hash.unwrap()).read() {
+            Entry::File { content } => print!("{}", content),
+            Entry::Tree { .. } => unimplemented!(),
+        },
 
         "hash-object" => {
-            let mut content_suffix = fs::read_to_string(&args.file_path.unwrap())
-                .unwrap()
-                .as_bytes()
-                .to_vec();
-            let mut content = format!("blob {}\0", content_suffix.len())
-                .as_bytes()
-                .to_vec();
-            content.append(&mut content_suffix);
+            let content = read_file_into_encoded_blob(&args.file_path.unwrap());
 
             let mut hasher = Sha1::new();
             hasher.update(&content);
-            let hash = hasher.finalize();
-            let hash_str = bytes_to_string(&hash[..]);
+            let hash = Hash::new(bytes_to_string(&hasher.finalize()));
 
-            println!("{}", hash_str);
+            println!("{}", hash.hash);
 
-            let prefix = &hash_str[0..2];
-            let filename = &hash_str[2..];
-            let folderpath_raw = format!(".git/objects/{}", prefix);
-            let folderpath = Path::new(&folderpath_raw);
-            fs::create_dir_all(folderpath).unwrap();
+            fs::create_dir_all(hash.folder_path()).unwrap();
 
             let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
             encoder.write_all(&content).unwrap();
             let content_encoded = encoder.finish().unwrap();
 
-            let filepath_raw = format!(".git/objects/{}/{}", prefix, filename);
-            let filepath = Path::new(&filepath_raw);
-            fs::write(filepath, content_encoded).unwrap();
+            fs::write(hash.file_path(), content_encoded).unwrap();
         }
+
+        "ls-tree" => match Hash::new(args.object_hash.unwrap()).read() {
+            Entry::File { .. } => unimplemented!(),
+            Entry::Tree { entries } => {
+                for entry in entries {
+                    println!(
+                        "{} {} {}\t{}",
+                        entry.perm,
+                        entry.perm_to_string(),
+                        entry.hash.hash,
+                        entry.filename
+                    );
+                }
+            }
+        },
 
         other => {
             error!("unknown command: {}", other)
